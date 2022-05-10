d89ec6149a892c3b7466152fd09f3505542a5bca
Greg Bouzianis <greg.bouz@gmail.com>
2022-05-10 08:19:11 -0400

    adds simple client and server console projects to communicate on specified port

---
+++ src/Raft.Client/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Socket _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+ConnectToServer();
+var exit = false;
+while (!exit)
+{
+    Console.WriteLine("Enter a command (type exit to quit): ");
+    string command = Console.ReadLine();
+    if (command.ToLower() == "exit")
+    {
+        exit = true;
+    }
+    Send(command);
+    WaitForResponse();
+}
+
+void ConnectToServer()
+{
+    int attempts = 0;
+    while (!_clientSocket.Connected)
+    {
+        try
+        {
+            attempts++;
+            _clientSocket.Connect(IPAddress.Loopback, 8000);
+        }
+        catch (SocketException e)
+        {
+            Console.Clear();
+            Console.WriteLine("Connection attempts: " + attempts.ToString());
+        }
+    }
+
+    Console.WriteLine("Client Connected");
+}
+
+void WaitForResponse()
+{
+    var buffer = new byte[2048];
+    int received = _clientSocket.Receive(buffer, SocketFlags.None);
+    if (received == 0) return;
+    var data = new byte[received];
+    Array.Copy(buffer, data, received);
+    string text = Encoding.ASCII.GetString(data);
+    Console.WriteLine(text);
+}
+
+void Send(string command)
+{
+    byte[] buffer = Encoding.UTF8.GetBytes(command);
+    _clientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+}
---
+++ src/Raft.Server/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+if (args.Length == 0 || string.IsNullOrEmpty(args[0]) || !int.TryParse(args[0], out int result))
+{
+    throw new ArgumentException("A valid port number must be provided.");
+}
+
+int port = result;
+byte[] _buffer = new byte[1024];
+List<Socket> _clientSockets = new List<Socket>();
+Socket _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+_serverSocket.Bind(new IPEndPoint(IPAddress.Any, port));
+_serverSocket.Listen();
+_serverSocket.BeginAccept(new AsyncCallback(OnAccept), null);
+Console.ReadLine();
+
+void OnAccept(IAsyncResult asyncResult)
+{
+    Socket socket = _serverSocket.EndAccept(asyncResult);
+    _clientSockets.Add(socket);
+    socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(OnReceive), socket);
+    _serverSocket.BeginAccept(new AsyncCallback(OnAccept), null);
+}
+
+void OnReceive(IAsyncResult asyncResult)
+{
+    Socket socket = (Socket)asyncResult.AsyncState;
+    int received = socket.EndReceive(asyncResult);
+    byte[] dataBuffer = new byte[received];
+    Array.Copy(_buffer, dataBuffer, received);
+
+    string text = Encoding.UTF8.GetString(dataBuffer);
+    Console.WriteLine("Test received: " + text);
+
+    byte[] data = Encoding.UTF8.GetBytes(text);
+    socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(OnSend), socket);
+    socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(OnReceive), socket);
+}
+
+static void OnSend(IAsyncResult asyncResult)
+{
+    Socket socket = (Socket)asyncResult.AsyncState;
+    socket.EndSend(asyncResult);
+}
